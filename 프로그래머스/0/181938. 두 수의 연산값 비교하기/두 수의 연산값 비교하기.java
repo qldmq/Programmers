e28550acff3fea4d3015925085a0ea152a783a03
qldmq <ksh13345@naver.com>
2025-03-13 19:06:31 +0900

    [level 0] Title: 두 수의 연산값 비교하기

---
+++ 프로그래머스/0/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int a, int b) {
+        int answer = 0;
+
+        int ab = Integer.parseInt(String.valueOf(a) + String.valueOf(b));
+        int ab2 = 2 * a * b;
+
+        if (ab > ab2) {
+            answer = ab;
+        } else {
+            answer = ab2;
+        }
+
+        return answer;
+    }
+}
