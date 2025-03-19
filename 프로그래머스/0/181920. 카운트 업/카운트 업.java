f6524d8a357476ec28c5f1b526e87937b3e90b6c
ksh <117808380+qldmq@users.noreply.github.com>
2025-03-20 00:26:57 +0900

    [level 0] Title: 카운트 업, Time: 0.03 ms, Memory: 97.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] solution(int start_num, int end_num) {
+        int[] answer = new int[end_num - start_num + 1];
+        int num = start_num;
+        
+        for (int i=0; i<=end_num-start_num; i++) {
+            answer[i] = num;
+            num++;
+        }
+        
+        return answer;
+    }
+}
