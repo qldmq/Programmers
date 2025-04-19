970bf25c242147f7f90408a2e6dc660f59c99c6b
ksh <117808380+qldmq@users.noreply.github.com>
2025-04-19 18:45:00 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.03 ms, Memory: 84.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        
+        for (int i : numbers) {
+            answer += i;
+        }
+        
+        return 45 - answer;
+    }
+}
