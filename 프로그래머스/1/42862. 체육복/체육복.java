8e393fe6987d25fd6ef68515d5cbcbdae3892a0b
ksh <117808380+qldmq@users.noreply.github.com>
2025-04-22 19:42:07 +0900

    [level 1] Title: 체육복, Time: 0.16 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -3,34 +3,33 @@ import java.util.*;
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
         int answer = 0;
-        int borrow = 0;
-        List<Integer> reserveList = new ArrayList<>();
         Set<Integer> lostSet = new HashSet<>();
-
-        for (int i : reserve) {
-            reserveList.add(i);
-        }
+        Set<Integer> reserveSet = new HashSet<>();
 
         for (int i : lost) {
-            if (reserveList.contains(i)) {
-                borrow++;
-                reserveList.remove((Integer) i);
+            lostSet.add(i);
+        }
+        
+        for (int i : reserve) {
+            if (lostSet.contains(i)) {
+                lostSet.remove(i);
             } else {
-                lostSet.add(i);
+                reserveSet.add(i);
             }
         }
 
-        for (int i : lostSet) {
-            if (reserveList.contains(i - 1)) {
-                borrow++;
-                reserveList.remove((Integer)(i - 1));
-            } else if (reserveList.contains(i + 1)) {
-                borrow++;
-                reserveList.remove((Integer)(i + 1));
+        for (int i : new HashSet<>(lostSet)) {
+            if (reserveSet.contains(i-1)) {
+                lostSet.remove(i);
+                reserveSet.remove((Integer) (i-1));
+            } else if (reserveSet.contains(i+1)) {
+                lostSet.remove(i);
+                reserveSet.remove((Integer) (i+1));
             }
         }
-
-        answer = n - (lost.length - borrow);
+       
+        int size = lostSet.size();
+        answer = n - size;
         
         return answer;
     }
