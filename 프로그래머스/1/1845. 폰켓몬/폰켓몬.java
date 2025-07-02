a2c1daf2fe16458890e20975cccf42118532a291
ksh <117808380+qldmq@users.noreply.github.com>
2025-07-02 16:56:22 +0900

    [level 1] Title: 폰켓몬, Time: 4.11 ms, Memory: 83.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        int n = nums.length/2;
+        
+        Set<Integer> key = new HashSet<>();
+        
+        for (int i : nums) {
+            if (key.contains(i)) {
+                answer++;
+            } else {
+                key.add(i);
+            }
+            
+            answer = (key.size() < n) ? key.size() : n;
+        }
+        return answer;
+    }
+}
